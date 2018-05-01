e41f2cbc9d6ebc344faf7f5b55c2ab5e0742fd72
Patryk Bernasiewicz <kontakt@patrykb.pl>
2018-05-01 13:54:06 +0200

    Vets find in range controller

---
+++ src/controllers/vets.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const { MongoError } = require('mongodb');
+const User = mongoose.model('users');
+const Vet = mongoose.model('vets');
+const { ValidationError, NotFoundError } = require('../helpers/errors');
+
+exports.findInRange = async (req, res, next) => {
+  const { range, lat, lng } = req.params;
+  const vets = await Vet
+    .findWithinRange(range, lat, lng)
+    .catch(err => next(err));
+  if (!vets) {
+    return res.sendStatus(404);
+  }
+  return res.status(200).json({ vets });
+};
